4317e7fd884c7cc6a5c5ce4d43843381a6ba1a35
Heloisa Busquim <hbusquim@gmail.com>
2022-08-16 10:31:45 -0300

    Lessons 2 and 3

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -7,12 +7,56 @@ namespace ByteBank
 {
     public class ContaCorrente
     {
-        public string titular {get; set;} = "";
-        public string numero_conta {get; set;} = "";
+        public string titular {get; set;}
+        public string numero_conta {get; set;}
         public int numero_agencia {get; set;}
-        public string nome_agencia {get; set;} = "";
+        public string nome_agencia {get; set;}
         public double saldo {get; set;}
 
+        public ContaCorrente(string titular, string numero_conta, int numero_agencia, string nome_agencia, double saldo)
+        {
+            this.titular = titular;
+            this.numero_conta = numero_conta;
+            this.numero_agencia = numero_agencia;
+            this.nome_agencia = nome_agencia;
+            this.saldo = saldo;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                Console.WriteLine("Saque não realizado. Saldo insuficiente!");
+                // Console.WriteLine("O valor a sacar é de: " + valor + ", porém o seu saldo atual é de: " + saldo);
+            }
+            else
+            {
+                saldo -= valor;
+                // Console.WriteLine("Saque realizado com sucesso da conta: " + numero_conta + ". Seu saldo atual é de: " + saldo);
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo +=valor;
+            Console.WriteLine("Depósito realizado com sucesso!");
+        }
+
+        public void Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                Console.WriteLine("Transferência não realizada. Saldo insuficiente!");
+                // Console.WriteLine("O valor a transferir é de: " + valor + ", porém o seu saldo é de: " + saldo);
+            }
+            else
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                // Console.WriteLine("Transferência realizada com sucesso da conta: " + numero_conta + " para a conta " + destino.numero_conta + ". Seu saldo atual é de: " + saldo);
+
+            }
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,16 +2,36 @@
 
 Console.WriteLine("Boas vindas ao banco ByteBank!");
 
-ContaCorrente conta1 = new ContaCorrente();
+ContaCorrente conta1 = new ContaCorrente("Heloisa Busquim", "12345-5", 123, "agencia central", 250.00);
+ContaCorrente conta2 = new ContaCorrente("Maria Eduarda", "12345-3", 234, "agencia norte", 300.00);
 
-    conta1.titular = "Heloisa Busquim";
-    conta1.numero_agencia = 0345;
-    conta1.numero_conta = "123345-6";
-    conta1.nome_agencia = "Campinas Bank";
-    conta1.saldo = 250.00;
+//Teste para operacoes sacar, depositar e transferir com valores abaixo do saldo atual
 
-    Console.WriteLine("Titular:" + conta1.titular);
-    Console.WriteLine("Agência:" + conta1.numero_agencia);
-    Console.WriteLine("Conta:" + conta1.numero_conta);
-    Console.WriteLine("Nome da agência:" + conta1.nome_agencia);
-    Console.WriteLine("Saldo:" + conta1.saldo);
+Console.WriteLine("Saldo anterior da conta: " + conta1.numero_conta + " é de: " + conta1.saldo);
+Console.WriteLine("Saldo anterior da conta: " + conta2.numero_conta + " é de: " + conta2.saldo);
+
+conta1.Sacar(50); //conta1 = 200, conta2 = 300
+conta2.Depositar(100); //conta1 = 200, conta2 = 400
+conta2.Transferir(150, conta1); //conta1 = 350, conta2 = 250
+
+Console.WriteLine("Saldo atual da conta: " + conta1.numero_conta + " é de: " + conta1.saldo);
+Console.WriteLine("Saldo atual da conta: " + conta2.numero_conta + " é de: " + conta2.saldo);
+
+
+ContaCorrente conta3 = new ContaCorrente("Felipe Duarte", "12345-6", 145, "agencia sul", 50.00);
+ContaCorrente conta4 = new ContaCorrente("Natasha Alencar", "12345-9", 567, "agencia leste", 100.00);
+
+Console.WriteLine("********************************************");
+
+
+//Teste para operacoes sacar, depositar e transferir com valores acima do saldo atual
+
+Console.WriteLine("Saldo anterior da conta: " + conta3.numero_conta + " é de: " + conta3.saldo);
+Console.WriteLine("Saldo anterior da conta: " + conta4.numero_conta + " é de: " + conta4.saldo);
+
+conta3.Sacar(300); //conta1 = 50, conta2 = 100
+conta4.Depositar(50); //conta1 = 50, conta2 = 150
+conta4.Transferir(400, conta1); //conta1 = 50, conta2 = 150
+
+Console.WriteLine("Saldo atual da conta: " + conta3.numero_conta + " é de: " + conta3.saldo);
+Console.WriteLine("Saldo atual da conta: " + conta4.numero_conta + " é de: " + conta4.saldo);
